b7280f91f9429db726f38eada3dd4b801c6b15d0
roman_awais <roman0812a@gmail.com>
2026-02-10 07:27:56 +0530

    Time: 8 ms (38.42%) | Memory: 47.4 MB (26.89%) - LeetSync

---
+++ 2767-maximum-sum-with-exactly-k-elements/maximum-sum-with-exactly-k-elements.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximizeSum(int[] nums, int k) {
+        Arrays.sort(nums);
+        int t=nums[nums.length-1],ans=0;
+        for(int i=0;i<k;i++){
+            ans+=t;
+            t=t+1;
+        }
+        return ans;
+    }
+}
