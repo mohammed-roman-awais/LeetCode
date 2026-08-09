46c8ee2df33919958aacb4b870d39c83c60a1387
roman_awais <roman0812a@gmail.com>
2026-08-09 21:56:43 +0530

    Time: 4 ms (100%), Space: 45.5 MB (98.26%) - LeetHub

---
+++ 2739-total-distance-traveled/2739-total-distance-traveled.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int distanceTraveled(int mainTank, int additionalTank) {
+        int distance = 0;
+        while (mainTank >= 5) {
+            int fuel = 5;
+            mainTank -= 5;
+            distance += 50;
+            if (additionalTank > 0) {
+                mainTank++;
+                additionalTank--;
+            }
+        }
+        distance += mainTank * 10;
+        return distance;
+    }
+}
