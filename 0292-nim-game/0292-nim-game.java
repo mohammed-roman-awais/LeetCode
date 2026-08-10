7084a4a6dbfd14b03bf53da8c7270089b7667c06
roman_awais <roman0812a@gmail.com>
2026-08-10 11:19:48 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.7%) - LeetHub

--- 0292-nim-game/0292-nim-game.java
+++ 0292-nim-game/0292-nim-game.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean canWinNim(int n) {
-        return n%4!=0;
+        // return n%4!=0;
+        int stones=n,maxTake=3;
+        return stones % (maxTake + 1) != 0;
     }
 }
