6e97539a78586e82ece3c7fc7731d00e0f4e771a
roman_awais <roman0812a@gmail.com>
2026-08-10 16:56:55 +0530

    Time: 0 ms (100%), Space: 42.1 MB (52.05%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -1,11 +1,5 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        int i=1;
-        while(true){
-            if(i%2==0 && i%n==0){
-                return i;
-            }
-            i++;
-        }
+        return n % 2 == 0 ? n : n * 2;
     }
 }
